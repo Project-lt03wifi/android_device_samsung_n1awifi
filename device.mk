d16ac5a3a5e75227e27048422e1041c24959f71e
deadman96385 <seanhoyt963@gmail.com>
2016-10-01 20:05:01 -0500

    Cleanup and update ramdisk

--- device.mk
+++ device.mk
@@ -189,7 +189,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGES += \
     libnetcmdiface \
-    macloader
+    macloader \
+	wifiloader
 
 # call dalvik heap config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
